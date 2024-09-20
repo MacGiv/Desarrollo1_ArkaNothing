0fd9ee4271f6a8e2966df5dea63ba6a479a28b56
MacGiv <tomasluchelli.17@gmail.com>
2024-09-20 10:42:08 -0300

    Putting project together

---
+++ Desarrollo1_Arkanot/src/main.cpp
@@ -0,0 +1,30 @@
+#include "sl.h"
+
+int main(int args, char* argv[])
+{
+    int screenWidth = 1024;
+    int screenHeight = 768;
+
+    int paddleHeight = 50;
+    int paddleWidth = 150;
+
+    // set up our window and a few resources we need
+    slWindow(screenWidth, screenHeight, "Simple SIGIL Example", false);
+
+
+
+
+    while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+    {
+        // background glow
+        slSetForeColor(1.0, 1.0, 1.0, 1.0);
+
+
+        slRectangleFill(screenWidth / 2, screenHeight / 2, paddleWidth, paddleHeight);
+
+
+        slRender();
+    }
+    slClose();
+    return 0;
+}
